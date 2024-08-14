2a2ec869e446c91d82b3ab919aa87924494706c3
amanamard <amanamard@gmail.com>
2024-08-14 01:19:17 -0400

    Add security project with Spring Boot

---
+++ SpringSecurityAuth/src/main/java/com/openclassrooms/repository/DBUserRepository.java
@@ -0,0 +1,10 @@
+package com.openclassrooms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.openclassrooms.model.DBUser;
+
+public interface DBUserRepository extends JpaRepository<DBUser, Integer> {
+    public DBUser findByUsername(String username);
+    
+}
